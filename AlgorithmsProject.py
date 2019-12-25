fddebffc4cc62e8215d8a010765beebdfd404420
AvivaShooman <vavass18@gmail.com>
2019-12-25 16:08:39 -0500

    Fixed Algorithm

--- AlgorithmsProject.py
+++ AlgorithmsProject.py
@@ -21,7 +21,8 @@ def clean_text(filename):
 
     print("The name of the file is", filename)
     print("The number of characters our file contains:", count)
-
+    print(newString)
+    
     return newString
 
 #Rabin Karp, https://www.geeksforgeeks.org/rabin-karp-algorithm-for-pattern-searching/
@@ -33,7 +34,7 @@ def search(pat, txt, q):
     p = 0	# hash value for pattern
     t = 0	# hash value for txt
     h = 1
-    d = 26 # d is 26 here because only lowercase letters
+    d = 26 #Here because we use lowercase letters as our character set
     
     # d is the size of the character set (26? 128? 256?)
     # q is used for modulo, generally a prime number
@@ -41,41 +42,46 @@ def search(pat, txt, q):
     # Initialize variables for counting operations
     count_m = 0
     count_n = 0
-
+    
     # The value of h would be "pow(d, n-1)%q"
     for i in range(n-1):
         h = (h*d)%q
   
     # Calculate the hash value of pattern and first window of text
     for i in range(n):
+
         p = (d*p + ord(pat[i]))%q
         t = (d*t + ord(txt[i]))%q
-        
- # Slide the pattern over text one by one
+    
+
+    # Slide the pattern over text one by one
     for i in range(m-n+1):
-        count_m += 1
+        # Count the number of hash value comparisons
+        count_m += 1 
         # Compare hash of current window in text with that of pattern; if match, do one by one 
         if p==t:
             # Check for characters one by one
             for j in range(n):
+                # Count the number of letter by letter comparisons
+                count_n += 1
                 if txt[i+j] != pat[j]:
                     break
-                else:
-                    count_n += 1
+                
             j+=1
             # if p == t and pat[0...n-1] = txt[i, i+1, ...i+n-1]
             if j==n:
                 print("Pattern found at index " + str(i))
   
         # Calculate hash value for next window of text: Remove leading digit, add trailing digit
-        if i < n-m:
+        if i < m-n:  
+
             t = (d*(t-ord(txt[i])*h) + ord(txt[i+n]))%q
             # We might get negative values of t, converting it to positive
             if t < 0:
                 t = t+q
                 
-    return count_m * count_n
-
+    return count_m + count_n
+    
 #Knuth-Morris-Pratt, https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
 def computeLPSArray(pat, n, lps):
     len = 0 # length of the previous longest prefix suffix
@@ -132,12 +138,12 @@ def KMPSearch(pat, txt):
     return count_n + count_m
 
 def main():
-    
-    text_files = [('./10_words.txt', 1), ('./100_words.txt', 2), ('./1000_words.txt', 3), ('./10000_words.txt', 4)]
+    #Set up test data
     pat_list1 = ['art']
     pat_list2 = ['daysandyears']
     pat_list3 = ['greatcity']
     pat_list4 = ['art', 'daysandyears', 'greatcity', 'heavenward']
+    text_files = [('./10_words.txt', pat_list1), ('./100_words.txt', pat_list2), ('./1000_words.txt', pat_list3), ('./10000_words.txt', pat_list4)]
     ans1 = 0
     ans2 = 0
 
@@ -147,15 +153,16 @@ def main():
         
         # Driver program to test Rabin-Karp 
         txt = clean_text(File[0])
-        pat = "pat_list" + str(File[1])
         q = 101 # A prime number
-        for pattern in pat:
-            print("The pattern is", pattern)
-            ans1 = search(pattern, txt, q)
+        pat = File[1]
+        
+        for i in range(len(pat)):
+            print("The pattern is", pat[i])
+            ans1 = search(pat[i], txt, q)
             print("The number of comparisons RK is", ans1)
 
             # Driver program to test Knuth-Morris Pratt
-            ans2 = KMPSearch(pattern, txt)
+            ans2 = KMPSearch(pat[i], txt)
             print("The number of comparisons KMP is", ans2)
 
 main()
